e9fd5bef3d844efab11370744770ad2e224d2abe
Daniel James <djames@syncromatics.com>
2017-03-24 11:03:45 -0700

    feat(routes): Added support for getting routes for a customer (EN-1531)

--- src/mocks.js
+++ src/mocks.js
@@ -72,6 +72,41 @@ export const charlie = {
   },
 };
 
+export const routes = {
+  setUpSuccessfulMock: (client) => {
+    const listResponse = () => new Response(
+      toBlob(routes.list),
+      {
+        headers: {
+          Link: '</1/SYNC/routes?page=1&perPage=10&q=blue&sort=>; rel="next", </1/SYNC/routes?page=1&perPage=10&q=blue&sort=>; rel="last"',
+        },
+      });
+    const singleResponse = () => new Response(toBlob(routes.getById(1)));
+
+    fetchMock
+      .get(client.resolve('/1/SYNC/routes?page=1&perPage=10&q=blue&sort='), listResponse)
+      .get(client.resolve('/1/SYNC/routes/1'), singleResponse);
+  },
+  getById: id => routes.list.find(v => v.id === id),
+  list: [
+    {
+      href: '/1/SYNC/routes/1',
+      id: 1,
+      name: 'Blue Line',
+      short_name: 'Blue',
+      description: 'Servicing the Townsville community',
+      is_public: true,
+      color: '#0000FF',
+      text_color: '#FFFFFF',
+      patterns: [
+        {
+          href: '/1/SYNC/patterns/1',
+        },
+      ],
+    },
+  ],
+};
+
 export const signs = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
