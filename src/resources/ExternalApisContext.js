72ae36e91aa52a309793694f5cdb1df81d8bb0cf
Jeff Koch <jrkoch@users.noreply.github.com>
2017-07-17 14:04:36 -0700

    feat(external_apis): Added support for new external_apis endpoint in Track API (EN-1254) (#16)

---
+++ src/resources/ExternalApisContext.js
@@ -0,0 +1,48 @@
+import 'isomorphic-fetch';
+import PagedContext from './PagedContext';
+import ExternalApi from './ExternalApi';
+
+/**
+ * ExternalApi querying context
+ *
+ * This is used to query the list of external APIs for a customer
+ */
+class ExternalApisContext extends PagedContext {
+  /**
+   * Creates a new route context
+   * @param {Client} client Instance of pre-configured client
+   * @param {string} customerCode Customer code
+   * @param {Object} params Object of querystring parameters to append to the URL
+   */
+  constructor(client, customerCode, params) {
+    super(client, { ...params });
+    this.code = customerCode;
+  }
+
+  /**
+   * Sets the query term for the context
+   * @example
+   * const externalApis = new ExternalApisContext(...);
+   * externalApis
+   *   .withQuery('arrivals')
+   *   .getPage()
+   *   .then(page => ...);
+   * @param {string} term Query term to search for
+   * @returns {ExternalApisContext} Returns itself
+   */
+  withQuery(term) {
+    this.params.q = term;
+    return this;
+  }
+
+  /**
+   * Gets the first page of results for this context
+   * @returns {Promise} If successful, a page of ExternalApi objects
+   * @see ExternalApi
+   */
+  getPage() {
+    return this.page(ExternalApi, `/1/${this.code}/external_apis`);
+  }
+}
+
+export default ExternalApisContext;
