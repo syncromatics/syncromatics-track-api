02711e0405fc781cdbaf24f42c0f8dc778c8ac78
Jeff Koch <jkoch@syncromatics.com>
2017-11-10 17:47:53 -0800

    Add Agency resource to customer

--- src/mocks.js
+++ src/mocks.js
@@ -75,6 +75,34 @@ export const charlie = {
   },
 };
 
+export const agencies = {
+  setUpSuccessfulMock: (client) => {
+    const sync = agencies.getByCode('SYNC');
+    const singleResponse = () => new Response(Client.toBlob(sync));
+    const putResponse = () => new Response(undefined, {
+      headers: {
+        Location: '/1/SYNC',
+      },
+    });
+
+    fetchMock
+      .get(client.resolve('/1/SYNC'), singleResponse)
+      .put(client.resolve('/1/SYNC'), putResponse);
+  },
+  getByCode: code => agencies.list.find(a => a.code === code),
+  list: [
+    {
+      href: '/1/SYNC',
+      code: 'SYNC',
+      name: 'Syncromatics Transit Agency',
+      agency_url: 'https://www.syncromatics.com/',
+      agency_phone: '310.728.6997',
+      agency_fare_url: 'https://www.syncromatics.com/about/',
+      agency_email: 'info@syncromatics.com',
+    },
+  ],
+};
+
 export const externalApis = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
