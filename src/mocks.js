f9450da86b71b1776941311bb0b1f4729af0e9e9
Daniel James <djames@syncromatics.com>
2017-11-19 14:46:01 -0800

    fix(dx): formatted mocks file

--- src/mocks.js
+++ src/mocks.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import fetchMock from 'fetch-mock';
-import { Server } from 'mock-socket';
+import {
+  Server
+} from 'mock-socket';
 /* eslint-enable import/no-extraneous-dependencies */
 import Client from './Client';
 import * as messages from './subscriptions/messages';
@@ -90,24 +92,21 @@ export const agencies = {
       .put(client.resolve('/1/SYNC'), putResponse);
   },
   getByCode: code => agencies.list.find(a => a.code === code),
-  list: [
-    {
-      href: '/1/SYNC',
-      code: 'SYNC',
-      name: 'Syncromatics Transit Agency',
-      agency_url: 'https://www.syncromatics.com/',
-      agency_phone: '310.728.6997',
-      agency_fare_url: 'https://www.syncromatics.com/about/',
-      agency_email: 'info@syncromatics.com',
-    },
-  ],
+  list: [{
+    href: '/1/SYNC',
+    code: 'SYNC',
+    name: 'Syncromatics Transit Agency',
+    agency_url: 'https://www.syncromatics.com/',
+    agency_phone: '310.728.6997',
+    agency_fare_url: 'https://www.syncromatics.com/about/',
+    agency_email: 'info@syncromatics.com',
+  }, ],
 };
 
 export const externalApis = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
-    Client.toBlob(externalApis.list),
-      {
+      Client.toBlob(externalApis.list), {
         headers: {
           Link: '</1/external_apis?page=1&per_page=10&q=arr&sort=>; rel="next", </1/external_apis?page=1&per_page=10&q=arr&sort=>; rel="last"',
         },
@@ -119,21 +118,18 @@ export const externalApis = {
       .get(client.resolve('/1/external_apis/1'), singleResponse);
   },
   getById: id => externalApis.list.find(e => e.id === id),
-  list: [
-    {
-      id: 1,
-      name: 'arrivals',
-      description: 'upcoming arrivals',
-      href: '/1/external_apis/1',
-    },
-  ],
+  list: [{
+    id: 1,
+    name: 'arrivals',
+    description: 'upcoming arrivals',
+    href: '/1/external_apis/1',
+  }, ],
 };
 
 export const messageTemplates = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
-      Client.toBlob(messageTemplates.list),
-      {
+      Client.toBlob(messageTemplates.list), {
         headers: {
           Link: '</1/SYNC/message_templates?page=1&per_page=10&q=5k&sort=>; rel="next", </1/SYNC/message_templates?page=1&per_page=10&q=5k&sort=>; rel="last"',
         },
@@ -152,57 +148,42 @@ export const messageTemplates = {
       .put(client.resolve('/1/SYNC/message_templates/1'), createResponse);
   },
   getById: id => messageTemplates.list.find(v => v.id === id),
-  list: [
-    {
-      href: '/1/SYNC/message_templates/1',
+  list: [{
+    href: '/1/SYNC/message_templates/1',
+    id: 1,
+    name: '5k Detour',
+    text: 'Due to the 5k Race, buses will detour off Figueroa from 6pm to 11am on 2/15/17. Find northbound buses on Hope, southbound buses on Flower.',
+    start: '2017-02-12T08:00:00-08:00',
+    end: '2017-02-15T11:00:00-08:00',
+    manual_archive_date: null,
+    sign_messages: [{
       id: 1,
-      name: '5k Detour',
-      text: 'Due to the 5k Race, buses will detour off Figueroa from 6pm to 11am on 2/15/17. Find northbound buses on Hope, southbound buses on Flower.',
-      start: '2017-02-12T08:00:00-08:00',
-      end: '2017-02-15T11:00:00-08:00',
-      manual_archive_date: null,
-      sign_messages: [
-        {
-          id: 1,
-          override_text: 'Due to the 5k, buses will be on detour.',
-          schedules: [
-            {
-              day_of_week: 1,
-              start: '06:00:00',
-              end: '18:00:00',
-            },
-          ],
-          tags: [
-            {
-              href: '/1/SYNC/tags/1',
-            },
-          ],
-          routes: [
-            {
-              href: '/1/SYNC/routes/1',
-            },
-          ],
-          stops: [
-            {
-              href: '/1/SYNC/stops/1',
-            },
-          ],
-          signs: [
-            {
-              href: '/1/SYNC/signs/1',
-            },
-          ],
-        },
-      ],
-    },
-  ],
+      override_text: 'Due to the 5k, buses will be on detour.',
+      schedules: [{
+        day_of_week: 1,
+        start: '06:00:00',
+        end: '18:00:00',
+      }, ],
+      tags: [{
+        href: '/1/SYNC/tags/1',
+      }, ],
+      routes: [{
+        href: '/1/SYNC/routes/1',
+      }, ],
+      stops: [{
+        href: '/1/SYNC/stops/1',
+      }, ],
+      signs: [{
+        href: '/1/SYNC/signs/1',
+      }, ],
+    }, ],
+  }, ],
 };
 
 export const routes = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
-      Client.toBlob(routes.list),
-      {
+      Client.toBlob(routes.list), {
         headers: {
           Link: '</1/SYNC/routes?page=1&per_page=10&q=blue&sort=>; rel="next", </1/SYNC/routes?page=1&per_page=10&q=blue&sort=>; rel="last"',
         },
@@ -214,23 +195,19 @@ export const routes = {
       .get(client.resolve('/1/SYNC/routes/1'), singleResponse);
   },
   getById: id => routes.list.find(v => v.id === id),
-  list: [
-    {
-      href: '/1/SYNC/routes/1',
-      id: 1,
-      name: 'Blue Line',
-      short_name: 'Blue',
-      description: 'Servicing the Townsville community',
-      is_public: true,
-      color: '#0000FF',
-      text_color: '#FFFFFF',
-      patterns: [
-        {
-          href: '/1/SYNC/patterns/1',
-        },
-      ],
-    },
-  ],
+  list: [{
+    href: '/1/SYNC/routes/1',
+    id: 1,
+    name: 'Blue Line',
+    short_name: 'Blue',
+    description: 'Servicing the Townsville community',
+    is_public: true,
+    color: '#0000FF',
+    text_color: '#FFFFFF',
+    patterns: [{
+      href: '/1/SYNC/patterns/1',
+    }, ],
+  }, ],
 };
 
 export const patterns = {
@@ -239,8 +216,7 @@ export const patterns = {
       Client.toBlob(patterns.list.map(pattern => ({
         ...pattern,
         stops: patterns.patternStopReferences,
-      }))),
-      {
+      }))), {
         headers: {
           Link: '</1/SYNC/patterns?page=1&per_page=10&q=blue&sort=>; rel="next", </1/SYNC/patterns?page=1&per_page=10&q=blue&sort=>; rel="last"',
         },
@@ -249,8 +225,7 @@ export const patterns = {
       Client.toBlob(patterns.list.map(pattern => ({
         ...pattern,
         stops: patterns.patternStops,
-      }))),
-      {
+      }))), {
         headers: {
           Link: '</1/SYNC/patterns?page=1&per_page=10&q=blue&sort=>; rel="next", </1/SYNC/patterns?page=1&per_page=10&q=blue&sort=>; rel="last"',
         },
@@ -263,27 +238,26 @@ export const patterns = {
       .get(client.resolve('/1/SYNC/patterns/1'), singleResponse);
   },
   getById: id => patterns.list
-    .map(p => ({ ...p, stops: patterns.patternStopReferences }))
+    .map(p => ({ ...p,
+      stops: patterns.patternStopReferences
+    }))
     .find(p => p.id === id),
-  list: [
-    {
-      id: 1,
-      route: {
-        href: '/1/SYNC/routes/1',
-      },
-      name: 'anttest',
-      short_name: "a's'l",
-      description: 'test',
-      color: '#4DB8FF',
-      text_color: '#FF142C',
-      length: 28356.700271064834,
-      direction_type: 'Loop',
-      encoded_polyline: 'miaoElhmqUeDlA??gDlA??aBl@v@mE??v@mE??x@mE??v@mE??v@mE??x@mE??v@mE??r@wDyA}D??wA{D??yA{D??wA}D??wA{D??{@}BPwE??NwE??PwE??NwE??NwE??PwE??NwE??PwE??NwE??PwE??NwE??NwE??PwE??NwE??PwE??NwE??PwE??NwE??NwE??PwE??NwE??PwE??NwE??PwE??NwE??PwE??NwE??NwE??JgCAwE??AyE??AwE??CyE??AwE??AyE??AwE??AyE??jAcE??|BrBd@`Ao@qE??o@oE??q@qE??i@{DLwE??NwE??BaAnDj@??nDj@??nDj@??nDj@??nDl@??nDj@??nDj@??nDj@??nDj@??vCd@AyE??sDI??sD@??EwE??pDE??rD???CyEsDO??qD???EyE??pDKrDA??DyE???G??qD@??sD???t@iE~@iE??~@kE??~@iE??~@iE??~@iE??`AkE??~@iE??~@iE??~@kE??~@iE??~@iE??`AiE??~@kE??~@iE??~@iE??~@iE??~@kE??~@iE??`AiE??~@kE??~@iE??~@iE??~@iE??~@kE??~@iE??`AiE??~@iE??~@kE??~@iE??~@iE??~@iE??r@cDxA{D??xA{D??xA{D??xA{D??xA{D??xA{D??xA{D??xA{D??vA{D??xA{D??xA{D??xA{D??xA{D??xA{D??xAyD??xA{D??xA{D??vA{D??xA{D??xA{D??xA{D??xA{D??xA{D??bAmCbCrC??`CrC??bCrC??bCrC??`CrC??bCrC??bCrC??`CrC??bCrC??bCrC??`CrC??bCrC??`CpCuB`D??wB`D??wB`D??uB`D??wB`D??uAtByAxD??yAzD??{AzD??yAzD??yAxD??yAzD??yAzD??{AxD??yAzD??yAzD??yAzD??yAxD??q@pE??e@rEIt@c@rE??e@tE??c@rE??c@rE??c@rE??c@rE??e@tE??c@rE??c@rE??c@rE??c@rE??e@tE??c@rE??c@rE??c@rE??c@rE??e@rE??c@tE??c@rE??c@rE??c@rE??e@rE??c@tE??c@rE??c@rE??c@rE??e@rE??c@tE??c@rE??c@rE??]lD??wB~C??wB~C??yB`D??wB~C??wB`D??yB~C??wB`D??wB~C??sBzCbBpD??dBrD??bBrD??bBrD??dBpD??bBrD??dBrD??bBpD??dBrD??bBrD??fA~B??@vE???xE??@vE???xE??@vE??@xE???vE??@vE???xE??@vE??@xE???vE??DxE??BvE?DDxE??BvE??DxE??@dC??c@rE??c@rE??e@rE??c@tE??c@rE??e@rE??c@rE??c@rE??e@rE??c@tE??c@rE??e@rE??c@rE??c@rE??c@rE??e@tE??c@rE??c@rE??e@rE??MtAqDX??qDZ??sDX??qDZ??qDX??qDZ??cAHsDD??qDF??sDF??sDD??sDF??sDF??kDDsDC??sDE??sDE??sDE??qDC??sDE??m@A',
-      href: '/1/SYNC/patterns/1',
+  list: [{
+    id: 1,
+    route: {
+      href: '/1/SYNC/routes/1',
     },
-  ],
-  patternStopReferences: [
-    {
+    name: 'anttest',
+    short_name: "a's'l",
+    description: 'test',
+    color: '#4DB8FF',
+    text_color: '#FF142C',
+    length: 28356.700271064834,
+    direction_type: 'Loop',
+    encoded_polyline: 'miaoElhmqUeDlA??gDlA??aBl@v@mE??v@mE??x@mE??v@mE??v@mE??x@mE??v@mE??r@wDyA}D??wA{D??yA{D??wA}D??wA{D??{@}BPwE??NwE??PwE??NwE??NwE??PwE??NwE??PwE??NwE??PwE??NwE??NwE??PwE??NwE??PwE??NwE??PwE??NwE??NwE??PwE??NwE??PwE??NwE??PwE??NwE??PwE??NwE??NwE??JgCAwE??AyE??AwE??CyE??AwE??AyE??AwE??AyE??jAcE??|BrBd@`Ao@qE??o@oE??q@qE??i@{DLwE??NwE??BaAnDj@??nDj@??nDj@??nDj@??nDl@??nDj@??nDj@??nDj@??nDj@??vCd@AyE??sDI??sD@??EwE??pDE??rD???CyEsDO??qD???EyE??pDKrDA??DyE???G??qD@??sD???t@iE~@iE??~@kE??~@iE??~@iE??~@iE??`AkE??~@iE??~@iE??~@kE??~@iE??~@iE??`AiE??~@kE??~@iE??~@iE??~@iE??~@kE??~@iE??`AiE??~@kE??~@iE??~@iE??~@iE??~@kE??~@iE??`AiE??~@iE??~@kE??~@iE??~@iE??~@iE??r@cDxA{D??xA{D??xA{D??xA{D??xA{D??xA{D??xA{D??xA{D??vA{D??xA{D??xA{D??xA{D??xA{D??xA{D??xAyD??xA{D??xA{D??vA{D??xA{D??xA{D??xA{D??xA{D??xA{D??bAmCbCrC??`CrC??bCrC??bCrC??`CrC??bCrC??bCrC??`CrC??bCrC??bCrC??`CrC??bCrC??`CpCuB`D??wB`D??wB`D??uB`D??wB`D??uAtByAxD??yAzD??{AzD??yAzD??yAxD??yAzD??yAzD??{AxD??yAzD??yAzD??yAzD??yAxD??q@pE??e@rEIt@c@rE??e@tE??c@rE??c@rE??c@rE??c@rE??e@tE??c@rE??c@rE??c@rE??c@rE??e@tE??c@rE??c@rE??c@rE??c@rE??e@rE??c@tE??c@rE??c@rE??c@rE??e@rE??c@tE??c@rE??c@rE??c@rE??e@rE??c@tE??c@rE??c@rE??]lD??wB~C??wB~C??yB`D??wB~C??wB`D??yB~C??wB`D??wB~C??sBzCbBpD??dBrD??bBrD??bBrD??dBpD??bBrD??dBrD??bBpD??dBrD??bBrD??fA~B??@vE???xE??@vE???xE??@vE??@xE???vE??@vE???xE??@vE??@xE???vE??DxE??BvE?DDxE??BvE??DxE??@dC??c@rE??c@rE??e@rE??c@tE??c@rE??e@rE??c@rE??c@rE??e@rE??c@tE??c@rE??e@rE??c@rE??c@rE??c@rE??e@tE??c@rE??c@rE??e@rE??MtAqDX??qDZ??sDX??qDZ??qDX??qDZ??cAHsDD??qDF??sDF??sDD??sDF??sDF??kDDsDC??sDE??sDE??sDE??qDC??sDE??m@A',
+    href: '/1/SYNC/patterns/1',
+  }, ],
+  patternStopReferences: [{
       href: '/1/SYNC/patterns_stops/11652',
     },
     {
@@ -302,8 +276,7 @@ export const patterns = {
       href: '/1/SYNC/patterns_stops/11668',
     },
   ],
-  patternStops: [
-    {
+  patternStops: [{
       href: '/1/SYNC/patterns_stops/11652',
       stop: {
         href: '/1/SYNC/stops/1',
@@ -381,8 +354,7 @@ export const patterns = {
 export const signs = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
-      Client.toBlob(signs.list),
-      {
+      Client.toBlob(signs.list), {
         headers: {
           Link: '</1/SYNC/signs?page=1&per_page=10&q=first&sort=>; rel="next", </1/SYNC/signs?page=1&per_page=10&q=first&sort=>; rel="last"',
         },
@@ -394,21 +366,18 @@ export const signs = {
       .get(client.resolve('/1/SYNC/signs/1'), singleResponse);
   },
   getById: id => signs.list.find(v => v.id === id),
-  list: [
-    {
-      href: '/1/SYNC/signs/1',
-      id: 1,
-      name: 'The first sign',
-      enabled: true,
-    },
-  ],
+  list: [{
+    href: '/1/SYNC/signs/1',
+    id: 1,
+    name: 'The first sign',
+    enabled: true,
+  }, ],
 };
 
 export const stops = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
-      Client.toBlob(stops.list),
-      {
+      Client.toBlob(stops.list), {
         headers: {
           Link: '</1/SYNC/stops?page=1&per_page=10&q=1st&sort=>; rel="next", </1/SYNC/stops?page=1&per_page=10&q=1st&sort=>; rel="last"',
         },
@@ -432,22 +401,19 @@ export const stops = {
       .put(client.resolve('/1/SYNC/stops/1'), putResponse);
   },
   getById: id => stops.list.find(v => v.id === id),
-  list: [
-    {
-      href: '/1/SYNC/stops/1',
-      id: 1,
-      name: '1st/Main',
-      latitude: 34.081728,
-      longitude: -118.351585,
-    },
-  ],
+  list: [{
+    href: '/1/SYNC/stops/1',
+    id: 1,
+    name: '1st/Main',
+    latitude: 34.081728,
+    longitude: -118.351585,
+  }, ],
 };
 
 export const tags = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
-      Client.toBlob(tags.list),
-      {
+      Client.toBlob(tags.list), {
         headers: {
           Link: '</1/SYNC/tags?page=1&per_page=10&q=LA&sort=>; rel="next", </1/SYNC/tags?page=1&per_page=10&q=LA&sort=>; rel="last"',
         },
@@ -471,21 +437,18 @@ export const tags = {
       .put(client.resolve('/1/SYNC/tags/3'), putResponse);
   },
   getById: id => tags.list.find(v => v.id === id),
-  list: [
-    {
-      href: '/1/SYNC/tags/3',
-      id: 3,
-      name: 'DTLA',
-      customerId: 1,
-    },
-  ],
+  list: [{
+    href: '/1/SYNC/tags/3',
+    id: 3,
+    name: 'DTLA',
+    customerId: 1,
+  }, ],
 };
 
 export const users = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
-      Client.toBlob(users.list),
-      {
+      Client.toBlob(users.list), {
         headers: {
           Link: '</1/users?page=1&per_page=10&q=1st&sort=>; rel="next", </1/users?page=1&per_page=10&q=1st&sort=>; rel="last"',
         },
@@ -498,24 +461,21 @@ export const users = {
       .get(client.resolve('/1/users/me'), singleResponse);
   },
   getById: id => users.list.find(v => v.id === id),
-  list: [
-    {
-      href: '/1/users/1',
-      id: 1,
-      preferences: {
-        track: {
-          homepage: '/trk',
-        },
+  list: [{
+    href: '/1/users/1',
+    id: 1,
+    preferences: {
+      track: {
+        homepage: '/trk',
       },
     },
-  ],
+  }, ],
 };
 
 export const vehicles = {
   setUpSuccessfulMock: (client) => {
     const listResponse = () => new Response(
-      Client.toBlob(vehicles.list),
-      {
+      Client.toBlob(vehicles.list), {
         headers: {
           Link: '</1/SYNC/vehicles?page=1&per_page=10&q=12&sort=>; rel="next", </1/SYNC/vehicles?page=1&per_page=10&q=12&sort=>; rel="last"',
         },
@@ -527,31 +487,41 @@ export const vehicles = {
       .get(client.resolve('/1/SYNC/vehicles/1'), singleResponse);
   },
   getById: id => vehicles.list.find(v => v.id === id),
-  list: [
-    {
-      href: '/1/SYNC/vehicles/1',
-      id: 1,
-      name: '1234',
-      enabled: true,
-      capacity: 40,
-      assignment: {
-        vehicle: { href: '/1/SYNC/vehicles/1' },
-        driver: { href: '/1/SYNC/drivers/1' },
-        pattern: { href: '/1/SYNC/patterns/1' },
-        run: { href: '/1/SYNC/runs/1' },
-        trip: { href: '/1/SYNC/trips/1' },
-        start: '2017-01-01T00:00:00.000-07:00',
-        sign_in_type: 'Dispatch',
-        on_break: false,
+  list: [{
+    href: '/1/SYNC/vehicles/1',
+    id: 1,
+    name: '1234',
+    enabled: true,
+    capacity: 40,
+    assignment: {
+      vehicle: {
+        href: '/1/SYNC/vehicles/1'
       },
+      driver: {
+        href: '/1/SYNC/drivers/1'
+      },
+      pattern: {
+        href: '/1/SYNC/patterns/1'
+      },
+      run: {
+        href: '/1/SYNC/runs/1'
+      },
+      trip: {
+        href: '/1/SYNC/trips/1'
+      },
+      start: '2017-01-01T00:00:00.000-07:00',
+      sign_in_type: 'Dispatch',
+      on_break: false,
     },
-  ],
+  }, ],
 };
 
 const realTimeUri = 'ws://localhost:8083/1/realtime';
 export const realTime = {
   uri: realTimeUri,
-  options: { realTimeUri },
+  options: {
+    realTimeUri
+  },
   authenticatedClient: {
     authenticated: Promise.resolve(),
   },
@@ -603,11 +573,19 @@ export const realTime = {
      */
     server.verifySubscription = (entityName, options = {}) => {
       let resolver;
-      const promise = new Promise((resolve) => { resolver = resolve; });
+      const promise = new Promise((resolve) => {
+        resolver = resolve;
+      });
       server.onTrackMessage(messages.SUBSCRIPTION_START.REQUEST, (data) => {
         if (data.entity === entityName) {
           // eslint-disable-next-line no-unused-vars
-          const { type, customer, entity, request_id, ...rest } = data;
+          const {
+            type,
+            customer,
+            entity,
+            request_id,
+            ...rest
+          } = data;
 
           if (options.closeConnection) {
             // it's important to close the realtime client before the server,
@@ -629,7 +607,9 @@ export const realTime = {
 
     // automatically accept any authentication request our test server receives.
     server.onTrackMessage(messages.AUTHENTICATION.REQUEST, () => {
-      const response = JSON.stringify({ type: messages.AUTHENTICATION.SUCCESS });
+      const response = JSON.stringify({
+        type: messages.AUTHENTICATION.SUCCESS
+      });
       server.emit('message', response);
 
       let subscriptionIdCounter = 0;
@@ -654,6 +634,13 @@ export const realTime = {
               } = vehicle;
               return rest;
             });
+            break;
+          case 'ASSIGNMENTS':
+            data = vehicles.list.map(v => v.assignment);
+            break;
+          case 'STOPTIMES':
+            console.warn('Need to define mocks for STOPTIMES');
+            data = [];
             break;
           default:
             throw new Error(`Don't know what to emit for ${request.entity}`);
