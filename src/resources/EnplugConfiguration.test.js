f83852f19f1db1313d18da35101cff192a579849
k-grant <38666098+k-grant@users.noreply.github.com>
2019-12-02 10:56:10 -0800

    feat(enplugScreenshotAndConfiguration): Add calls for getting screenshots and updating config (#70)
    * feat(enplugScreenshotAndConfiguration): Add calls for getting an enplug screenshot and updating configuration
    
    * mock put call for configuration update
    
    * mock response empty
    
    * add customer helper functions for usage
    
    * remove hash

---
+++ src/resources/EnplugConfiguration.test.js
@@ -0,0 +1,54 @@
+import chai from 'chai';
+import fetchMock from 'fetch-mock';
+
+import chaiAsPromised from 'chai-as-promised';
+import Client from '../Client';
+import EnplugConfiguration from './EnplugConfiguration';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('When instantiating an enplug configuration based on customer and enplug serial', () => {
+  const client = new Client();
+  const enplugConfiguration = new EnplugConfiguration(client, EnplugConfiguration.makeHref('SYNC', 'SERIAL11'));
+
+  it('should set the href', () => enplugConfiguration.href.should.equal('/1/SYNC/enplugs/SERIAL11/configuration'));
+  it('should not be hydrated', () => enplugConfiguration.hydrated.should.equal(false));
+});
+
+describe('When instantiating a vehicle media based on an object', () => {
+  const client = new Client();
+  const mockConfiguration = new EnplugConfiguration(client, { deviceSerial: 'LLCM2', volume: 28, shouldSwitch: true, vehicleHref: '/1/SYNC/vehicles/1234' });
+  const enplugConfiguration = new EnplugConfiguration(client, mockConfiguration);
+
+  it('should set the volume', () => enplugConfiguration.volume.should.equal(28));
+  it('should set the deviceSerial', () => enplugConfiguration.deviceSerial.should.equal('LLCM2'));
+  it('should set the vehicleHref', () => enplugConfiguration.vehicleHref.should.equal('/1/SYNC/vehicles/1234'));
+  it('should set the shouldSwitch', () => enplugConfiguration.shouldSwitch.should.equal(true));
+});
+
+describe('When updating enplug configuration', () => {
+  const client = new Client();
+  const mockConfiguration = new EnplugConfiguration(client, {
+    href: '/1/SYNC/enplugs/LLCM2/configuration',
+    code: 'SYNC',
+    deviceSerial: 'LLCM2',
+    volume: 28,
+    shouldSwitch: true,
+    vehicleHref: '/1/SYNC/vehicles/1234' });
+
+  const singleResponse = () => new Response();
+  beforeEach(() => {
+    fetchMock.put(client.resolve('/1/SYNC/enplugs/LLCM2/configuration'), singleResponse);
+  });
+  beforeEach(() => fetchMock.catch(503));
+
+  let promise;
+  beforeEach(() => {
+    promise = new EnplugConfiguration(client, mockConfiguration).update();
+  });
+
+  it('should resolve the promise', () => promise.should.be.fulfilled);
+  it('should set the volume', () => promise.then(v => v.volume).should.eventually.equal(28));
+  it('should set the deviceSerial', () => promise.then(v => v.deviceSerial).should.eventually.equal('LLCM2'));
+});
