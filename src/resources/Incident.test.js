8fbd2d88fdbf0773596343251ba6d41a84fc0b94
Daniel James <daniel@thzinc.com>
2020-06-23 10:28:17 -0700

    fix(incidents): Fix error in posting body for incident actions
    - Added dispose() and deprecated disposeIncident()

--- src/resources/Incident.test.js
+++ src/resources/Incident.test.js
@@ -52,7 +52,7 @@ describe('When add note to an incident', () => {
   it('should resolve the promise', () => promise.should.be.fulfilled);
 });
 
-describe('When dispose an incident', () => {
+describe('When disposing an incident', () => {
   const client = new Client();
   beforeEach(() => mockIncidents.setUpSuccessfulMock(client));
   beforeEach(() => fetchMock.catch(503));
@@ -61,11 +61,11 @@ describe('When dispose an incident', () => {
   });
 
   it('should resolve the promise with false alarm', () => {
-    const promise = new Incident(client, Incident.makeHref('SYNC', 1)).disposeIncident(1);
+    const promise = new Incident(client, Incident.makeHref('SYNC', 1)).dispose('FalseAlarm');
     return promise.should.be.fulfilled;
   });
   it('should resolve the promise with Cleared', () => {
-    const promise = new Incident(client, Incident.makeHref('SYNC', 1)).disposeIncident(2);
+    const promise = new Incident(client, Incident.makeHref('SYNC', 1)).dispose('Cleared');
     return promise.should.be.fulfilled;
   });
 });
