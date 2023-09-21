87bfb54753f3a4c6323aa3d5f5a4bbf2e755e2cf
glen <gperry@gmvsync.com>
2023-09-21 10:48:07 -0700

    Revert "discard"
    This reverts commit 570afb3ae17eed5e641445bc4eb7c793cf73d4b0.

--- src/mocks/tripCancelations.js
+++ src/mocks/tripCancelations.js
@@ -16,16 +16,11 @@ const tripCancelations = {
                     Link: '</1/SYNC/serviceadjustments/cancelations?page=1&per_page=10&sort=>; rel="next", </1/SYNC/areas?page=1&per_page=100&sort=>; rel="last"'
                 },
             });
-        const postResponse = () => new Response(Client.toBlob(tripCancelations.list), {
-            headers: {
-                Location: '/1/SYNC/serviceadjustments/cancelations',
-            },
-        });
 
         fetchMock
             .get(client.resolve('/1/SYNC/serviceadjustments/cancelations?page=1&per_page=100&sort='), listResponsePaged)
+        fetchMock
             .get(client.resolve('/1/SYNC/serviceadjustments/cancelations?page=1&per_page=10&sort='), listResponseNext)
-            .post(client.resolve('/1/SYNC/serviceadjustments/cancelations'), postResponse)
     },
     list: [{
         id: 1,
