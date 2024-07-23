1de5b7d00fa5bee4af60c6122a3b454279ade6db
Cooper Scott <william.scott@gmv.com>
2024-07-23 08:36:55 -0500

    update detour test mocks

--- src/mocks/detour.js
+++ src/mocks/detour.js
@@ -4,46 +4,77 @@ import Client from '../Client';
 
 const detours = {
     setUpSuccessfulMock: (client) => {
-        const singleResponse = () => new Response(Client.toBlob(detours.list));
+        const singleResponse = () => new Response(Client.toBlob(detours.response));
 
         fetchMock
             .get(client.resolve('/1/SYNC/serviceadjustments/detours'), singleResponse)
-            .post(client.resolve('/1/SYNC/serviceadjustments/detours'), (newDetour) => new Response(Client.toBlob([...detours.list, newDetour])))
-            .delete(client.resolve(`/1/SYNC/serviceadjustments/detours/2`), {
-              status: 200,
-              body: Client.toBlob(detours.list.filter(detour => detour.detour_id !== 2))
-            });
+            .post(client.resolve('/1/SYNC/serviceadjustments/detours'), () => singleResponse())
+            .delete(client.resolve(`/1/SYNC/serviceadjustments/detours/2`), () => singleResponse())
     },
-    list: [
-        {
-            detour_id: 2,
-            customer_id: 123,
-            pattern_id: 456,
-            detour_pattern_id: 789,
-            title: 'A Detour',
-            should_match_scheduled_stops: true,
-            creator_user_id: 101,
-            created_date_time: new Date().toISOString(),
-            deactivator_user_id: null,
-            deactivated_date_time: null,
-            start_date_time: new Date('2023-01-01T09:00:00Z').toISOString(),
-            end_date_time: new Date('2023-01-15T17:00:00Z').toISOString(),
+    response: {
+        client: {
+            options: {
+                baseUri: "https://track-api.syncromatics.com"
+            },
+            jwt: {
+                token: "token-here",
+                header: {
+                    alg: "HS256",
+                    typ: "JWT"
+                },
+                claim: {
+                    // Claims
+                },
+                signature: "ABC123"
+            },
+            authenticated: {}
         },
-        {
-            detour_id: 3,
-            customer_id: 123,
-            pattern_id: 323,
-            detour_pattern_id: 433,
-            title: 'Another Detour',
-            should_match_scheduled_stops: true,
-            creator_user_id: 101,
-            created_date_time: new Date().toISOString(),
-            deactivator_user_id: null,
-            deactivated_date_time: null,
-            start_date_time: new Date('2023-01-01T09:00:00Z').toISOString(),
-            end_date_time: new Date('2023-01-15T17:00:00Z').toISOString(),
+        hydrated: true,
+        detours: {
+            "96": [
+                {
+                    detour_id: 189,
+                    customer_id: 1,
+                    pattern_id: 96,
+                    detour_pattern_id: 44420,
+                    title: "Detour created from Mock Tests.",
+                    should_match_scheduled_stops: true,
+                    creator_user_id: 397,
+                    created_date_time: "2024-07-22T16:45:57.168336+00:00",
+                    deactivator_user_id: 397,
+                    deactivated_date_time: "2024-07-22T18:58:29+00:00",
+                    start_date_time: "2024-07-23T01:00:00-07:00",
+                    end_date_time: "2024-07-23T01:30:00-07:00"
+                },
+                {
+                    detour_id: 200,
+                    customer_id: 1,
+                    pattern_id: 96,
+                    detour_pattern_id: 44420,
+                    title: "Detour created from Mock Tests.",
+                    should_match_scheduled_stops: true,
+                    creator_user_id: 397,
+                    created_date_time: "2024-07-22T16:45:57.168336+00:00",
+                    start_date_time: "2024-07-23T01:00:00-07:00",
+                    end_date_time: "2024-07-23T01:30:00-07:00"
+                }
+            ],
+            "40062": [
+                {
+                    detour_id: 194,
+                    customer_id: 1,
+                    pattern_id: 40062,
+                    detour_pattern_id: 12206,
+                    title: "Detour created from Mock Tests.",
+                    should_match_scheduled_stops: true,
+                    creator_user_id: 397,
+                    created_date_time: "2024-07-23T12:55:25.8659593+00:00",
+                    start_date_time: "2024-07-23T01:00:00-07:00",
+                    end_date_time: "2024-07-23T01:15:00-07:00"
+                }
+            ]
         }
-    ]
+    }
 };
 
 export default detours;
